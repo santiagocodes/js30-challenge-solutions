8f93f83322426a45bb397cf9cbf7c337d87dea64
mc-santiago <mari.c.santiago@europe.com>
2020-05-16 22:28:58 +0200

    animate.css and intro page basics done

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        default: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+    },
+    extend: {
+      spacing: {
+        "10p": "10%",
+        "20p": "20%",
+        "30p": "30%",
+        "40p": "40%",
+        "50p": "50%",
+        "60p": "60%",
+        "70p": "70%",
+        "80p": "80%",
+        "90p": "90%",
+        "100p": "100%",
+        "5vh": "5vh",
+        "10vh": "10vh",
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "50vh": "50vh",
+        "55vh": "55vh",
+        "60vh": "60vh",
+        "65vh": "65vh",
+        "70vh": "70vh",
+        "75vh": "75vh",
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+  },
+  plugins: [],
+};
